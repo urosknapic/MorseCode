ae23d09e971215f6ed3741a97eff66cc137ced13
urosknapic <uros.knapic@gmail.com>
2021-11-18 20:18:17 +0100

    Init push

---
+++ MorseCode/MorseCodeTests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace MorseCodeTests
+{
+  public class Tests
+  {
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+      Assert.Fail();
+    }
+  }
+}
