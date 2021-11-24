c2e6bd3608d00abe9cae58703d1adfd5c091e849
urosknapic <uros.knapic@gmail.com>
2021-11-24 20:12:31 +0100

    Testing getting length of sequence for dot and dash. and some cases

--- MorseCode/MorseCode/Program.cs
+++ MorseCode/MorseCode/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MorseCode
 {
@@ -6,7 +8,13 @@ namespace MorseCode
   {
     static void Main(string[] args)
     {
+      // ···· · −·−−   ·−−− ··− −·· ·
+      string inputByTwoUnits = "1100110011001100000011000000111111001100111111001111110000000000000011001111110011111100111111000000110011001111110000001111110011001100000011";
+      string inputByOneUnit = "1010101001001101011011000000010110110110010101100110101001";
+      string inputByNUnit = "110101011";
 
+      var array = inputByTwoUnits.Split("0", StringSplitOptions.RemoveEmptyEntries);
+      var dic = array.Distinct().ToDictionary(value => value, value => value.Length);
     }
   }
 }
